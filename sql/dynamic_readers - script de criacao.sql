28b265e031befc537771df01268df52101e36171
AlexandraHY14 <alexandra.yara@sptech.school>
2023-05-30 20:39:18 -0300

    atualização da Modelagem lógica e do script (criação das tabelas - livro e usuarioLivro)

--- sql/dynamic_readers - script de criacao.sql
+++ sql/dynamic_readers - script de criacao.sql
@@ -5,3 +5,37 @@ CREATE USER IF NOT EXISTS projeto_individual IDENTIFIED BY '12345';
 GRANT SELECT, INSERT, UPDATE, DELETE ON dynamic_readers.* TO '12345';
 FLUSH PRIVILEGES;
 
+CREATE DATABASE dynamic_readers;
+USE dynamic_readers;
+
+CREATE USER IF NOT EXISTS urubu100 IDENTIFIED BY 'urubu100';
+GRANT SELECT, INSERT, UPDATE, DELETE ON dynamic_readers.* TO 'urubu100';
+FLUSH PRIVILEGES;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nomeUsuario varchar(50),
+email varchar(50),
+senha varchar(40)
+);
+
+create table livro(
+idLivro int primary key auto_increment,
+nome varchar(50) not null,
+nomeAutor varchar(50),
+totalDePaginas int not null,
+dia int not null, -- ou decimal(2)
+mes varchar(10) not null,
+ano char(4) not null,
+qtdPagDia int,
+diaSemana varchar(15) not null,
+finalizado char(1),
+constraint chkfinalizado check (finalizado in ('n','s'))
+);
+
+create table usuarioLivro(
+fkUsuario int,
+fkLivro int,
+dataResumo date,
+resumo varchar(1500)
+);
